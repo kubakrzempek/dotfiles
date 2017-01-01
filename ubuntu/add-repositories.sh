3a848e696e5d0d965bb6827ed5d07041b491eb24
Kuba Krzempek <kubakrzempek@gmail.com>
2017-01-01 21:58:09 +0100

    Add nodejs repo
    The repo contains more recent version of nodejs than the
    universe one.

--- ubuntu/add-repositories.sh
+++ ubuntu/add-repositories.sh
@@ -15,5 +15,9 @@ echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) co
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 
+echo "> Adding NodeJS 6 repo..."
+echo "deb https://deb.nodesource.com/node_6.x $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+wget -q https://deb.nodesource.com/gpgkey/nodesource.gpg.key -O- | sudo apt-key add -
+
 echo "> Adding Atom repo..."
 sudo add-apt-repository ppa:webupd8team/atom
