13855b7b7c36d596d77c5f3e879ba810d08de1ef
Kuba Krzempek <kubakrzempek@gmail.com>
2017-11-07 19:25:33 +0100

    Update the repo to work with Ubuntu 17.10

--- ubuntu/add-repositories.sh
+++ ubuntu/add-repositories.sh
@@ -10,10 +10,5 @@ sudo apt-get -y install wget
 echo "> Adding multiverse..."
 sudo add-apt-repository multiverse
 
-echo "> Adding VirtualBox repo..."
-echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-
 echo "> Adding Atom repo..."
 sudo add-apt-repository ppa:webupd8team/atom
