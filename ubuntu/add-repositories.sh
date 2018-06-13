df81ccf044d77fa380cb74c58ea1f611c70f657e
kubakrzempek <kubakrzempek@gmail.com>
2018-06-13 11:39:53 +0200

    Add postgresql bionic (18.04) repository

--- ubuntu/add-repositories.sh
+++ ubuntu/add-repositories.sh
@@ -12,3 +12,7 @@ sudo add-apt-repository multiverse
 
 echo "> Adding Atom repo..."
 sudo add-apt-repository ppa:webupd8team/atom
+
+echo "> Adding PostgreSQL repo..."
+echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
